a0671e658953cb6c0731c7e6f5fa8c093f8e3a2f
afif86 <afif.hosseini86@gmail.com>
2022-01-10 23:53:01 +0200

    adding

--- src/Create.js
+++ src/Create.js
@@ -1,12 +1,35 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
     const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [schedule, setSchedule] = useState('');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const todo = { title, body, schedule };
+        setIsPending(true);
+        
+        fetch('http://localhost:8000/todos', {
+            method: 'POST',
+            headers: { "content-Type": "application/json" },
+            body: JSON.stringify(todo)
+        }).then(() => {
+            console.log('new todo added');
+            setIsPending(false);
+            history.push('/');
+        })
+
+        
+    }
 
     return (
         <div className="create">
             <h2>Add a new To-Do </h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Title:</label>
                 <input
                     type="text"
@@ -18,16 +41,21 @@ const Create = () => {
                 <textarea
                     className="description"
                     type="text"
-                    
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
                 />
-                <label for="Deadline">Date:</label>
+                <label >schedule:</label>
                 <input
                     type="date"
                     id="start"
                     name="todo-start"
                     required
+                    value={schedule}
+                    onChange={(e) => setSchedule(e.target.value)}
                 />
-                <button>Add</button>
+                {!isPending && <button>Add</button>}
+                {isPending && <button>Add todo...</button>}
+                
             </form>
         </div>
     );
